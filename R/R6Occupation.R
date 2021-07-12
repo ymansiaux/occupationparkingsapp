a9681359b974b70cf66a973a776ea825f9ea800f
Yohann Mansiaux <y.mansiaux@bordeaux-metropole.fr>
2021-07-12 11:38:49 +0200

    Issue 29 closed Migration tidytable vers data table

--- R/R6Occupation.R
+++ R/R6Occupation.R
@@ -36,26 +36,28 @@ Occupation <- R6::R6Class(
     #' (application de la fonction summarise_by_time de timetk)
     #' @param time_unit pas d'aggreg (voir timetk)
     #' @param ... parametres additionels de floor_date
-    #' @import tidytable
-    #' @importFrom data.table :=
+    #' @import data.table
     #' @importFrom lubridate floor_date
     #' @examples \dontrun{ temporal_aggregate("day")
     #' } 
     mean_by_some_time_unit = function(time_unit, ...) {
-      self$aggregated_data_by_some_time_unit <- 
-        # on bind_rows : la moyenne globale et la moyenne par ident
-        bind_rows.(
+       # browser()
+      self$aggregated_data_by_some_time_unit <-
+        
+        rbind(
           self$cleaned_data %>% 
-            mutate.(time = floor_date(time, unit = time_unit, ...)) %>% 
-            summarise.(taux_occupation = mean(taux_occupation, na.rm = TRUE), .by = c(time)) %>% 
-            mutate.(ident = "moyenne", nom = "moyenne")
+            copy() %>% 
+            .[, time := floor_date(time, unit = time_unit, ...)] %>% 
+            .[, .(taux_occupation = mean(taux_occupation, na.rm = TRUE)), by = time] %>% 
+            .[, `:=`(ident = "moyenne", nom = "moyenne")]
           ,
-          
           self$cleaned_data %>% 
-            mutate.(time = floor_date(time, unit = time_unit, ...)) %>% 
-            summarise.(taux_occupation = mean(taux_occupation, na.rm = TRUE), .by = c(ident, nom, time))
+            copy() %>% 
+            .[, time := floor_date(time, unit = time_unit, ...)] %>% 
+            .[, .(taux_occupation = mean(taux_occupation, na.rm = TRUE)), by = list(ident, nom, time)]
         )
     },
+    
     
     #' @description
     #' Graphe de série temporelle
@@ -63,8 +65,7 @@ Occupation <- R6::R6Class(
     #' @importFrom ggplot2 ggplot aes geom_line scale_linetype_manual theme_minimal theme scale_color_manual
     #' @importFrom ggiraph geom_line_interactive geom_point_interactive
     #' @importFrom glue glue_data
-    #' @import tidytable
-    #' @importFrom data.table :=
+    #' @import data.table
     #' @importFrom lubridate hour
     #'  
     #' @examples \dontrun{ timeseries_plot(parkings_to_plot = c("A","B"))
@@ -72,21 +73,21 @@ Occupation <- R6::R6Class(
     timeseries_plot_1_period = function(parkings_to_plot) {
       
       self$data_plot_1_period <-  self$aggregated_data_by_some_time_unit %>% 
-        filter.(ident %in% c(parkings_to_plot, "moyenne")) %>% 
-        mutate.(tooltip = as.character(
+        copy() %>% 
+        .[ident %in% c(parkings_to_plot, "moyenne")] %>% 
+        .[, tooltip := as.character(
           glue_data(.SD, "Date : {as.character(time)}\nnom : {nom}\nVal : {sprintf('%.2f', taux_occupation)}")
-        )) %>% 
-        mutate.(linetype = ifelse(ident == "moyenne", "dotted", "solid"))
+        )] %>% 
+        .[, linetype := fifelse(ident == "moyenne", "dotted", "solid")]
       
-      
-      gg <- filter.(self$data_plot_1_period, ident %in% parkings_to_plot & ident != "moyenne") %>%  
+      gg <- self$data_plot_1_period[ident %in% parkings_to_plot & ident != "moyenne"] %>%  
         ggplot(data = ., mapping = aes(x = time, y = taux_occupation, color = nom, group=nom, linetype = nom)) + 
         geom_line_interactive(aes(data_id=ident), lwd = 1) + 
         geom_point_interactive(aes(tooltip=tooltip, data_id=ident)) + 
         theme_minimal() +
         theme(legend.position = "right") +
         
-        geom_line_interactive(data = self$data_plot_1_period %>% filter.(ident == "moyenne"), 
+        geom_line_interactive(data = self$data_plot_1_period[ident == "moyenne"], 
                               mapping = aes(x = time, y = taux_occupation, tooltip = tooltip, data_id = ident, group = nom, color = nom),
                               lwd = 1.5) +
         scale_linetype_manual(
@@ -94,9 +95,7 @@ Occupation <- R6::R6Class(
           values =
             unlist(
               with(
-                distinct.(self$data_plot_1_period %>% 
-                            filter.(ident %in% c("moyenne", parkings_to_plot)) %>%
-                            select.(nom, linetype)),
+                unique(self$data_plot_1_period[ident %in% c("moyenne", parkings_to_plot), c("nom", "linetype")]),
                 split(linetype, nom)))
         ) + 
         xlab("Heure") +
@@ -116,8 +115,7 @@ Occupation <- R6::R6Class(
     #' @importFrom ggplot2 ggplot aes geom_line scale_linetype_manual theme_minimal theme scale_color_manual
     #' @importFrom ggiraph geom_line_interactive geom_point_interactive
     #' @importFrom glue glue_data
-    #' @import tidytable
-    #' @importFrom data.table :=
+    #' @import data.table
     #' @importFrom lubridate hour wday day month
     #'  
     #' @examples \dontrun{ timeseries_plot(parkings_to_plot = c("A","B"), show_average = TRUE)
@@ -125,49 +123,51 @@ Occupation <- R6::R6Class(
     timeseries_plot_2_periods = function(data_occupation_1, data_occupation_2, timeStep, parkings_to_plot) {
       
       self$data_plot_2_periods <- data_occupation_1$aggregated_data_by_some_time_unit %>% 
-        mutate.(nom = paste0(nom, "_periode1")) %>% 
-        bind_rows.(data_occupation_2$aggregated_data_by_some_time_unit %>% 
-                     mutate.(nom = paste0(nom, "_periode2"))) %>% 
-        mutate.(tooltip = as.character(
-          glue_data(.SD, "Date : {as.character(time)}\nnom : {nom}\nVal : {sprintf('%.2f', taux_occupation)}")
-        )) %>% 
-        mutate.(linetype = ifelse(ident == "moyenne", "dotted", "solid"))  %>% 
-        filter.(ident %in% c(parkings_to_plot, "moyenne"))
-      
+        copy() %>% 
+        .[,nom := paste0(nom, "_periode1")] %>% 
+        rbind(., 
+              data_occupation_2$aggregated_data_by_some_time_unit %>% 
+                copy() %>% 
+              .[, nom := paste0(nom, "_periode2")]
+        ) %>% 
+        .[,tooltip := as.character(
+          glue_data(.SD, "Date : {as.character(time)}\nnom : {nom}\nTaux : {sprintf('%.2f', taux_occupation)}")
+        )] %>% 
+        .[,linetype := fifelse(ident == "moyenne", "dotted", "solid")]  %>% 
+        .[ident %in% c(parkings_to_plot, "moyenne")]
       
       if(timeStep == "Jour") {
-        # browser()
         self$data_plot_2_periods <- self$data_plot_2_periods %>% 
-          mutate.(time = strftime(time, "%H:%M"))
+          .[, time := strftime(time, "%H:%M")]
         
         xlab <- "Heure"
         
       } else if(timeStep == "Semaine") {
         self$data_plot_2_periods <- self$data_plot_2_periods %>% 
-          mutate.(time = factor(wday(time, label = TRUE, week_start = 1)))
+          .[, time := factor(wday(time, label = TRUE, week_start = 1))]
         
         xlab <- "Jour de la semaine"
         
       } else if(timeStep == "Mois") {
         self$data_plot_2_periods <- self$data_plot_2_periods %>% 
-          mutate.(time = factor(day(time)))
+          .[, time := factor(day(time))]
         
         xlab <- "Jour du mois"
         
       } else {
         self$data_plot_2_periods <- self$data_plot_2_periods %>% 
-          mutate.(time = factor(month(time, label = TRUE, abbr = FALSE)))
+          .[, time := factor(month(time, label = TRUE, abbr = FALSE))]
         
         xlab <- "Mois"
       }
       
-      gg <- filter.(self$data_plot_2_periods, ident %in% parkings_to_plot & ident != "moyenne") %>%  
+      gg <- self$data_plot_2_periods[ident %in% parkings_to_plot & ident != "moyenne"] %>%  
         ggplot(data = ., mapping = aes(x = time, y = taux_occupation, color = nom, group=nom, linetype = nom)) + 
         geom_line_interactive(aes(data_id=ident), lwd = 1) + 
         geom_point_interactive(aes(tooltip=tooltip, data_id=ident))  +
         theme_minimal() +
         
-        geom_line_interactive(data = self$data_plot_2_periods %>% filter.(ident == "moyenne"), 
+        geom_line_interactive(data = self$data_plot_2_periods[ident == "moyenne"], 
                               mapping = aes(x = time, y = taux_occupation, tooltip = tooltip, data_id = ident, group = nom, color = nom),
                               lwd = 1.5) +
         scale_linetype_manual(
@@ -175,9 +175,7 @@ Occupation <- R6::R6Class(
           values =
             unlist(
               with(
-                distinct.(self$data_plot_2_periods %>% 
-                            filter.(ident %in% c("moyenne", parkings_to_plot)) %>%
-                            select.(nom, linetype)),
+                unique(self$data_plot_2_periods[ident %in% c("moyenne", parkings_to_plot), c("nom", "linetype")]),
                 split(linetype, nom)))
         )  +
         xlab(xlab) +
