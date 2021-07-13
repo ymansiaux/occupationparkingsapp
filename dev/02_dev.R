21866fa6cddd33c9b15f489f75156209e4f1ef64
Yohann Mansiaux <y.mansiaux@bordeaux-metropole.fr>
2021-07-13 10:34:33 +0200

    Import theme en cours

--- dev/02_dev.R
+++ dev/02_dev.R
@@ -27,8 +27,7 @@ usethis::use_package( "glue")
 usethis::use_package( "purrr")
 usethis::use_package( "shinycssloaders")
 usethis::use_package( "shinybusy")
-
-
+usethis::use_package( "bdxmetroidentity")
 
 
 usethis::use_pipe()
