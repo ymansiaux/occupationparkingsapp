6701b8420d9d4b56afa02baa142fa4ae085a7df7
Yohann Mansiaux <y.mansiaux@bordeaux-metropole.fr>
2021-09-09 11:27:11 +0200

    Issue 47 closed

--- R/mod_occupation_1_periode_graphe.R
+++ R/mod_occupation_1_periode_graphe.R
@@ -20,7 +20,7 @@ mod_occupation_1_periode_graphe_ui <- function(id, title) {
     fluidRow(
       span(
         h4(title),
-        actionButton(inputId = ns("show_hide_panel"), label = "afficher / masquer le secteur", class = "btn btn-info")
+        actionButton(inputId = ns("show_hide_panel"), label = "afficher / masquer le secteur", class = "btn btn-info", style = "margin: 0 0 5% 0")
       )
     ),
     div(
@@ -46,11 +46,15 @@ mod_occupation_1_periode_graphe_ui <- function(id, title) {
         )
       ),
       fluidRow(
+        tags$span(
+          actionButton(inputId = ns("show_plot_data"), label = "Afficher / masquer les donn\u00e9es du graphe", class = "btn btn-warning", style = "margin: 0 0 5% 0"),
+          actionButton(inputId = ns("show_raw_data"), label = "Afficher / masquer les donn\u00e9es de la requ\u00eate", class = "btn btn-warning", style = "margin: 0 0 5% 0")
+        )
+      ),
+      fluidRow(
         column(
           width = 12,
-          lien_afficher_cacher_div(
-            id_lien = ns("show_plot_data"),
-            label_lien = "Afficher les donn\u00e9es du graphe",
+          hidden_div(
             id_div = ns("plot_data"),
             contenu_div = tagList(
               withSpinner(
@@ -59,20 +63,22 @@ mod_occupation_1_periode_graphe_ui <- function(id, title) {
             )
           )
         )
-      ),
-      fluidRow(column(
+      )
+    ),
+    fluidRow(
+      column(
         width = 12,
-        lien_afficher_cacher_div(
-          id_lien = ns("show_raw_data"),
-          label_lien = "Afficher les donn\u00e9es brutes",
+        hidden_div(
           id_div = ns("raw_data"),
           contenu_div = tagList(
-            withSpinner(
-              DTOutput(ns("table_raw"))
+            tagList(
+              withSpinner(
+                DTOutput(ns("table_raw"))
+              )
             )
           )
         )
-      ))
+      )
     )
   )
 }
@@ -84,26 +90,26 @@ mod_occupation_1_periode_graphe_server <- function(id, r6, app_theme, parkings_l
   moduleServer(id, function(input, output, session) {
     observe(updateSelectizeInput(session, "parkings_to_plot", choices = unique(r6$cleaned_data$nom), server = TRUE))
     # observeEvent(input$pause, browser())
-
+    
     observeEvent(input$show_hide_panel, {
       toggle(id = "show_results", anim = TRUE)
     })
-
+    
     output$plot <- renderGirafe({
       input$maj
-
+      
       validate(
         need(isTruthy(r6$data_xtradata), "Aucun graphe \u00e0 afficher - v\u00e9rifier la requ\u00eate")
       )
-
+      
       r6$aggregated_data_by_some_time_unit$nom[is.na(r6$aggregated_data_by_some_time_unit$nom)] <- "moyenne"
-
+      
       gg <- r6$timeseries_plot_1_period(
         parkings_to_plot = isolate(unique(parkings_list()$ident[parkings_list()$nom %in% input$parkings_to_plot])),
         timeStep = r6$timeStep,
         app_theme = app_theme()
       )
-
+      
       x <- girafe(
         ggobj = gg, width_svg = 8, height_svg = 5,
         pointsize = 15,
@@ -114,26 +120,26 @@ mod_occupation_1_periode_graphe_server <- function(id, r6, app_theme, parkings_l
       )
       x
     })
-
+    
     onclick(
       "show_plot_data",
       toggle(id = "plot_data", anim = TRUE)
     )
-
+    
     onclick(
       "show_raw_data",
       toggle(id = "raw_data", anim = TRUE)
     )
-
-
-
+    
+    
+    
     output$table_plot <- renderDT(server = FALSE, {
       input$maj
-
+      
       validate(
         need(isTruthy(r6$data_xtradata), "Aucun tableau \u00e0 afficher - v\u00e9rifier la requ\u00eate")
       )
-
+      
       r6$data_plot_1_period %>%
         .[, `:=`(
           taux_occupation = round(taux_occupation, 1),
@@ -143,16 +149,16 @@ mod_occupation_1_periode_graphe_server <- function(id, r6, app_theme, parkings_l
         .[, linetype := NULL] %>%
         .[, lwd := NULL] %>%
         datatable(.,
-          rownames = FALSE, caption = NULL,
-          extensions = "Buttons", options = parametres_output_DT
+                  rownames = FALSE, caption = NULL,
+                  extensions = "Buttons", options = parametres_output_DT
         )
     })
-
+    
     output$table_raw <- renderDT(server = FALSE, {
       validate(
         need(isTruthy(r6$data_xtradata), "Aucun tableau \u00e0 afficher - v\u00e9rifier la requ\u00eate")
       )
-
+      
       r6$cleaned_data %>%
         .[, `:=`(
           taux_occupation = round(taux_occupation, 1),
